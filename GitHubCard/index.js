12e4c1b944b115ac116033352e9113a5713df7b6
Warren Rawlings <rawlings.warren@gmail.com>
2019-08-16 10:15:47 -0400

    first commit from the video

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,13 +2,19 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+//const axios = require('axios');
+let user = {}
+axios.get('https://api.github.com/users/wtrawlings')
+    .then(response => console.log(response))
+    .catch(error => {
+        console.error(error)
+    })
+    /* Step 2: Inspect and study the data coming back, this is YOUR 
+       github info! You will need to understand the structure of this 
+       data in order to use it to build your component function 
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
-
-   Skip to Step 3.
-*/
+       Skip to Step 3.
+    */
 
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
@@ -45,6 +51,39 @@ const followersArray = [];
 </div>
 
 */
+//FIND HELP FROM JOE ALFARO IN THE SLACK HELP CHANNELS THIS WILL HELP. 
+//ALSO HE HAS VIDEOS RENDERING FOR YOUTUBE SO THERE WILL BE HELP THERE!
+
+const createCard = user => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const img = document.createElement('img');
+    img.src = user['avatar_url'];
+
+    const cardInfo = document.createElement('div');
+    cardInfo.classList.add('card-info');
+
+    const h3 = document.createElement('h3');
+    h3.classList.add('name');
+    h3.textContent = user.name || "See UserName";
+
+    for (let i = 0, i < 6, i++) {
+        pTags.push(document.createElement('p'));
+    }
+    pTags[0].classList.add('username');
+    pTags[0].textContent = user.login;
+
+    pTags[1].textContent = `location: ${user.location || "not available"}`;
+
+    pTags[2].textContent = `Profile: `;
+
+    const a = document.createElement('a');
+    const aUrl
+
+}
+
+
 
 /* List of LS Instructors Github username's: 
   tetondan
